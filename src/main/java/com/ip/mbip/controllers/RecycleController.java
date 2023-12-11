23087bf472b151831bdfc1b9d647e663bffaa831
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-12-11 10:57:44 +0800

    create recyclecontroller

---
+++ src/main/java/com/ip/mbip/controllers/RecycleController.java
@@ -0,0 +1,13 @@
+package com.ip.mbip.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RecycleController {
+
+    @GetMapping("/recyclePage")
+    public String showRecyclePage() {
+    return "recyclePage";
+    }
+}
