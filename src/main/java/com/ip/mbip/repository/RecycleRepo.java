10cf39a6d906ef952f53148efb69ae4a4808b5dc
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-12-21 00:13:29 +0800

    create repo recycle

---
+++ src/main/java/com/ip/mbip/repository/RecycleRepo.java
@@ -0,0 +1,9 @@
+package com.ip.mbip.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ip.mbip.model.Recycle;
+
+public interface RecycleRepo extends CrudRepository<Recycle,Long>{
+
+}
