1dff196a0e9fbd424ccf1ca6e4e7e836a129230a
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-12-21 00:13:41 +0800

    create service recycle

--- src/main/java/com/ip/mbip/service/RecycleService.java
+++ src/main/java/com/ip/mbip/service/RecycleService.java
@@ -1,5 +1,43 @@
 package com.ip.mbip.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ip.mbip.model.Recycle;
+import com.ip.mbip.repository.RecycleRepo;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class RecycleService {
+
+    @Autowired
+    private RecycleRepo recycleRepo;
+
+    public Iterable<Recycle> findAll(){
+        return recycleRepo.findAll();
+    }
+
+    public void addRecycle(Recycle recycle){
+        recycleRepo.save(recycle);
+    }
+
+    public void deleteById(long id){
+        recycleRepo.deleteById(id);
+    }
+
+    public void updateRecycle(Recycle recycle){
+        recycleRepo.save(recycle);
+    }
+
+    public Optional <Recycle> findById(long id){
+        return recycleRepo.findById(id);
+    }
+
+
+    
 
 }
