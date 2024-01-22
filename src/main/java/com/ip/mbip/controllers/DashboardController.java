aa683dba7b0a4bde366b2ffc6911066538375378
teenesh10 <teeneshsubramaniam10@gmail.com>
2024-01-22 23:10:31 +0800

    Added dynamic carbon footprint dashboard

--- src/main/java/com/ip/mbip/controllers/DashboardController.java
+++ src/main/java/com/ip/mbip/controllers/DashboardController.java
@@ -7,26 +7,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.ip.mbip.model.ElectricBill;
 import com.ip.mbip.model.Recycle;
 import com.ip.mbip.repository.RecycleRepo;
+import com.ip.mbip.service.ElectricService;
 import com.ip.mbip.service.RecycleService;
 
 @Controller
 public class DashboardController {
 
     @Autowired
-    private  RecycleService recycleService;
+    private RecycleService recycleService;
+
+    @Autowired
+    private ElectricService electricService;
 
     @GetMapping("/dashboard")
     public String showDashboard(Model model) {
-        // Iterable<Recycle> recycleList = recycleService.findAll(); // Assuming you have a service for retrieving data
+        // Iterable<Recycle> recycleList = recycleService.findAll(); // Assuming you
+        // have a service for retrieving data
         model.addAttribute("recycleList", recycleService.findAll());
+        Iterable<ElectricBill> electricBills = electricService.findAll();
+        model.addAttribute("electricBills", electricBills);
         return "dashboard";
     }
 
     @GetMapping("/blankfile")
     public String showBlank() {
-        // Iterable<Recycle> recycleList = recycleService.findAll(); // Assuming you have a service for retrieving data
+        // Iterable<Recycle> recycleList = recycleService.findAll(); // Assuming you
+        // have a service for retrieving data
         return "blankfile";
     }
 
